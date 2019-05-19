c2fc59f40f366de5931274b0039d4e58ec4a3494
boruto99 <30471188+boruto99@users.noreply.github.com>
2019-05-20 00:24:03 +0800

    Update Footer.js

--- src/Layout/Footer.js
+++ src/Layout/Footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
   return (
     <div className="footer">
  
-    <p>Powered by <a href="https://cosmicjs.com">CosmicJs</a> created by <a href="https://www.instagram.com">Dr.Emte</a> Inspired by <a href="https://medium.com/">Medium</a> </p>
+    <p>Powered by <a href="https://cosmicjs.com">CosmicJs</a> created by <a href="https://www.instagram.com/dr.emte">Dr.Emte</a> Inspired by <a href="https://medium.com/">Medium</a> </p>
     
      
     </div>
